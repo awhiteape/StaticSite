ab434e0115806006a6e4185cbf8481d03f3ae600
Jacob Lindholm <lindholmjacob@gmail.com>
2025-03-04 20:29:10 -0800

    Completed a node conversion function

--- src/text_node_to_html_node.py
+++ src/text_node_to_html_node.py
@@ -3,18 +3,28 @@ from leafnode import LeafNode
 from parentnode import ParentNode
 from textnode import TextNode, TextType
 
-def text_to_html(type):
-    if type == TextType.TEXT:
-        print("Text")
-    elif type == TextType.BOLD:
-        print("Bold")
-    elif type == TextType.ITALIC:
-        print("Italicized")
-    elif type == TextType.CODE:
-        print("Coded")
-    elif type == TextType.LINK:
-        print("Aaaaaaaa")
-    elif type == TextType.IMAGE:
-        print("Imagine")
+
+#takes a text_node argument and handles the conversion to HTML
+#check text_node class for arguments and swap type for text_node
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+
+    if text_node.text_type == TextType.TEXT:
+        new_leaf = LeafNode(None, text_node.text)
+        return new_leaf
+    elif text_node.text_type == TextType.BOLD:
+        new_leaf = LeafNode("b", text_node.text)
+        return new_leaf
+    elif text_node.text_type == TextType.ITALIC:
+        new_leaf = LeafNode("i", text_node.text)
+        return new_leaf
+    elif text_node.text_type == TextType.CODE:
+        new_leaf = LeafNode("code", text_node.text)
+        return new_leaf
+    elif text_node.text_type == TextType.LINK:
+        new_leaf = LeafNode("a", text_node.text, {"href":text_node.url})
+        return new_leaf
+    elif text_node.text_type == TextType.IMAGE:
+        new_leaf = LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        return new_leaf
     else:
         raise Exception("Type not defined as Enum in text_to_html() arg.")
