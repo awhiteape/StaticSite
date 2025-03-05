c4b1a37c2ccda87eee62a397b4270cf9057dc248
Jacob Lindholm <lindholmjacob@gmail.com>
2025-03-04 20:28:31 -0800

    Added a quick test in main for a text node to html function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 from htmlnode import HTMLNode
 from leafnode import LeafNode
 from parentnode import ParentNode
+from text_node_to_html_node import text_node_to_html_node
 
 def main():
     text_test = TextNode("This is a text node", "bold", "https://www.boot.dev")
@@ -19,5 +20,14 @@ def main():
         LeafNode(None, "Normal text"),
     ],)
     parent_node.to_html()
+    print("=======Text to html node test======")
+    node = TextNode("This is a text node", TextType.TEXT)
+    html_node = text_node_to_html_node(node)
+    print(html_node.to_html())
+    try:
+        text_node_to_html_node(-1)
+    except Exception as err:
+        print(err)
+
 
 main()
